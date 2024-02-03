2729c9c5e68d956a5b58c80866b0e1c78802cdeb
TyphdoidComa <tesselstrom@gmail.com>
2024-02-03 10:28:45 -0800

    UPdate Rive

--- src/components/OkyruLogo.tsx
+++ src/components/OkyruLogo.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Rive from 'rive-react-native';
+import Rive, { Alignment, Fit } from 'rive-react-native';
 
 const OkyruLogo = () => {
   return (
     <View style={styles.container}>
-      <Rive resourceName="okyru_logo_anim" />
+      <Rive resourceName={'okyru_logo_anim'} fit={Fit.Contain} alignment={Alignment.Center} />
     </View>
   );
 };
