11ce1af9db867cac2b69909cfa7cff451a3a5ef6
TyphdoidComa <tesselstrom@gmail.com>
2024-02-04 12:46:30 -0800

    Splashscreen update.

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,30 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { Text, StyleSheet, Animated, Image } from 'react-native';
+import { StyleSheet, Animated, Image } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import OkyruLogo from '../components/OkyruLogo'; // Import the okyruLogo component
-const SplashScreen = ({ navigation }: { navigation: any }) => {
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RouteProp } from '@react-navigation/native';
+// import Icon from './Icon';
+
+type SplashScreenStackParamList = {
+  Home: undefined; // No parameters expected for the Home screen
+  // ... other screens in the stack
+};
+
+type SplashScreenNavigationProp = StackNavigationProp<SplashScreenStackParamList, 'Home'>;
+
+type SplashScreenRouteProp = RouteProp<SplashScreenStackParamList, 'Home'>;
+
+interface SplashScreenProps {
+  navigation: SplashScreenNavigationProp;
+  route: SplashScreenRouteProp; // If you need to use the route prop
+}
+
+const SplashScreen: React.FC<SplashScreenProps> = ({ navigation }) => {
   const [fadeAnim] = useState(new Animated.Value(1)); // Initial value for opacity: 1
 
   useEffect(() => {
+    // Start the fade-out animation after a delay
     const timer = setTimeout(() => {
       Animated.timing(fadeAnim, {
         toValue: 0,
-        duration: 2000,
-        useNativeDriver: true,
-      }).start(() => navigation.replace('Home')); // Start the animation
-    }, 2000); // Wait for 3000 milliseconds = 3 seconds
+        duration: 1000,
+        useNativeDriver: true, // Add useNativeDriver for better performance
+      }).start(() => navigation.replace('Home')); // Navigate to 'Home' after animation ends
+    }, 3000); // Start the animation after 3 seconds
 
     // Clear timeout if the component is unmounted
     return () => clearTimeout(timer);
-  }, [navigation, fadeAnim]);
+  }, [navigation, fadeAnim]); // Add fadeAnim to dependency array
 
   return (
-    <LinearGradient colors={['#EBF3FA', '#DDE7F3', '#E5F0F9']} style={styles.container}>
+    <LinearGradient colors={['#F1F5F8', '#DDE7F3', '#E5F0F9']} style={styles.container}>
       <Animated.View style={{ ...styles.container, opacity: fadeAnim }}>
-        <Text style={styles.text}>Splash Screen</Text>
-        <OkyruLogo />
         <Image source={require('../assets/logos/logo.png')} style={styles.image} />
-        <Text style={styles.text}>Splash Screen</Text>
       </Animated.View>
     </LinearGradient>
   );
@@ -41,9 +56,6 @@ const styles = StyleSheet.create({
     width: 250, // Or the size you want
     height: 250, // Or the size you want
     marginBottom: 20,
-  },
-  text: {
-    fontSize: 24,
   },
 });
 
