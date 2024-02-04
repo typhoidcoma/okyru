83377c76f2c5965b32389b02cd168afa7de8cc18
TyphdoidComa <tesselstrom@gmail.com>
2024-02-03 18:26:06 -0800

    Added Lottie

--- src/components/OkyruLogo.tsx
+++ src/components/OkyruLogo.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import Rive, { Alignment, Fit } from 'rive-react-native';
+import { StyleSheet, View } from 'react-native';
+import AnimatedLottieView from 'lottie-react-native';
 
 const OkyruLogo = () => {
+  const logoStyle = {
+    width: 320,
+    height: 320,
+  };
+
   return (
-    <Rive
-      resourceName={'truck_v7.riv'}
-      fit={Fit.Fill}
-      alignment={Alignment.Center}
-      autoplay={true}
-      // artboardName="Logo"
-      // style={styles.logo}
-    />
+    <View style={styles.logo}>
+      <AnimatedLottieView
+        source={require('../assets/logos/Logo.json')}
+        autoPlay
+        loop
+        style={logoStyle}
+      />
+    </View>
   );
 };
 
@@ -19,6 +24,7 @@ const styles = StyleSheet.create({
   logo: {
     width: 320,
     height: 320,
+    // backgroundColor: 'white',
   },
 });
 
