ca75bbe3545db0378695bfdf087a9c13504c0d49
TyphdoidComa <tesselstrom@gmail.com>
2024-02-06 08:01:22 -0800

    Build config updates

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
     presets: ['module:@react-native/babel-preset'],
     plugins: [
         // ... your other plugins
-        '@babel/plugin-proposal-export-namespace-from',
+        // '@babel/plugin-proposal-export-namespace-from',
         'react-native-reanimated/plugin',
     ],
 };
