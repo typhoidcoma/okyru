0276c8576f7b1c9766455a6085bc33df7fc5825d
TyphdoidComa <tesselstrom@gmail.com>
2024-02-06 20:01:27 -0800

    Home screen tweaks.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,61 +1,79 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import CustomLinearGradient from '../components/CustomLinearGradient';
-import IconButton from '../components/IconButton';
-import CircularTimer from '../components/CircularTimer';
-import { GlobalStyles } from '../styles/GlobalStyles';
-import ModalScreen from '../screens/ModalScreen';
+import { StackNavigationProp } from '@react-navigation/stack'; // Importing StackNavigationProp for navigation
+import CustomLinearGradient from '../components/CustomLinearGradient'; // Importing CustomLinearGradient component
+import IconButton from '../components/IconButton'; // Importing IconButton component
+import CircularTimer from '../components/CircularTimer'; // Importing CircularTimer component
+import { GlobalStyles } from '../styles/GlobalStyles'; // Importing GlobalStyles for consistent styling
+import ModalScreen from '../screens/ModalScreen'; // Importing ModalScreen component
 
 type HomeScreenProps = {
-    navigation: StackNavigationProp<any>; // Use the correct type for navigation
+    navigation: StackNavigationProp<any>; // Define the type for navigation prop
 };
 
+// Define the HomeScreen component
 const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
-    const [modalVisible, setModalVisible] = useState(false); // State to manage modal visibility
-    const [modalOpened, setModalOpened] = useState(false); // State to track whether modal has been opened
+    // State to manage modal visibility
+    const [modalVisible, setModalVisible] = useState(false);
+    // State to track whether modal has been opened
+    const [modalOpened, setModalOpened] = useState(false);
 
+    // Function to handle timer done event
     const handleTimerDone = () => {
+        // Show the modal only if it hasn't been opened before
         if (!modalOpened) {
-            setModalVisible(true); // Show the modal only if it hasn't been opened before
-            setModalOpened(true); // Update the state to indicate that the modal has been opened
+            setModalVisible(true);
+            // Update the state to indicate that the modal has been opened
+            setModalOpened(true);
         }
     };
 
+    // Function to close the modal
     const closeModal = () => {
-        setModalVisible(false); // Close the modal
+        setModalVisible(false);
     };
 
+    // Function to reset the state to indicate that the modal hasn't been opened yet
     const resetModal = () => {
-        setModalOpened(false); // Reset the state to indicate that the modal hasn't been opened yet
+        setModalOpened(false);
     };
+
+    // Render the HomeScreen component
     return (
         <CustomLinearGradient style={styles.gradient}>
+            {/* Container for the main content */}
             <View style={styles.container}>
+                {/* Circular timer component */}
                 <CircularTimer
                     size={220}
                     strokeWidth={8}
-                    time={10}
+                    time={1200}
                     color="rgba(234,0,8,.65)"
-                    onTimerDone={() => handleTimerDone()}
-                    onReset={resetModal} // Pass the resetModal function to CircularTimer
+                    // Pass the function to handle timer done event
+                    onTimerDone={handleTimerDone}
+                    // Pass the function to reset modal state
+                    onReset={resetModal}
                 />
 
-                {modalVisible && <ModalScreen onClose={closeModal} />}
-
                 {/* Rest of your UI components */}
+                {/* IconButton for navigation */}
                 <IconButton
                     svgIconName="26_Menu"
                     width={80}
                     height={80}
+                    // Navigate to Settings screen on press
                     onPress={() => navigation.navigate('Settings')}
                 />
+                {/* Text component */}
                 <Text style={[GlobalStyles.text, styles.appName]}>okyru</Text>
             </View>
+            {/* ModalScreen component, conditionally rendered based on modalVisible state */}
+            {modalVisible && <ModalScreen onClose={closeModal} />}
         </CustomLinearGradient>
     );
 };
 
+// Define the styles for the HomeScreen component
 const styles = StyleSheet.create({
     gradient: {
         flex: 1,
@@ -71,4 +89,5 @@ const styles = StyleSheet.create({
     },
 });
 
+// Export the HomeScreen component as default
 export default HomeScreen;
